8d9c2ccb0fbfd39d253d07877198fac2cbd39e0e
Matt Hauser <matt.hauser.web@gmail.com>
2023-08-24 16:27:57 -0700

    work damnit

--- pages/order/stepfour.js
+++ pages/order/stepfour.js
@@ -293,7 +293,12 @@ export default function Step4() {
         >
           {" "}
         </Typography> */}
-          <form name="order" action="/index" method="POST" data-netlify="true">
+          <form
+            name="order"
+            action="/gallery"
+            method="POST"
+            data-netlify="true"
+          >
             <div
               style={{
                 flexBasis: "100%",
